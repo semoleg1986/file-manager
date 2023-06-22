e2366d8dc33bcb4775f53bbbc8682c06258f3216
Oleg Semenov <semoleg1986@gmail.com>
2023-06-22 23:12:20 +0300

    feat: update inputCommand

--- src/helper/inputCommand.js
+++ src/helper/inputCommand.js
@@ -1,11 +1,24 @@
 export const inputCommand = (input, myEmitter) => {
   const [command, ...args] = input.split(' ');
 
-  if (command === 'ls') {
-    myEmitter.emit('ls');
-  } else if (command === 'cd') {
-    myEmitter.emit('cd', args);
-  } else if (command === 'up') {
-    myEmitter.emit('up', args);
+  const command_list = ['ls', 'cd', 'up'];
+  //  нужно поймать ошибки и использовать brake
+  for (let i = 0; i < command_list.length; i++) {
+    if (command === command_list[i]) {
+      if (command === 'ls') {
+        myEmitter.emit('ls');
+      }
+      else {
+        myEmitter.emit(command, args)
+      }
+    }
   }
+
+  // if (command === 'ls') {
+  //   myEmitter.emit('ls');
+  // } else if (command === 'cd') {
+  //   myEmitter.emit('cd', args);
+  // } else if (command === 'up') {
+  //   myEmitter.emit('up', args);
+  // }
 };
