d35f4b16679626f2c79db5413f355791f01aa702
Oleg Semenov <semoleg1986@gmail.com>
2023-06-24 02:34:20 +0300

    refactor: update

--- src/helper/inputCommand.js
+++ src/helper/inputCommand.js
@@ -1,23 +1,20 @@
-export const inputCommand = (input, myEmitter) => {
-  const [command, ...args] = input.split(' ');
-  const command_list = ['ls', 'cd', 'up', 'cat', 'add', 'rm'];
-  //  нужно поймать ошибки и использовать brake
-  for (let i = 0; i < command_list.length; i++) {
-    if (command === command_list[i]) {
-      if (command === 'ls') {
-        myEmitter.emit('ls');
-      }
-      else {
-        myEmitter.emit(command, args)
-      }
-    }
-  }
+import { dispCurrentDir } from "./currentDir.js";
 
-  // if (command === 'ls') {
-  //   myEmitter.emit('ls');
-  // } else if (command === 'cd') {
-  //   myEmitter.emit('cd', args);
-  // } else if (command === 'up') {
-  //   myEmitter.emit('up', args);
-  // }
+export const inputCommand = (input, myEmitter, rl) => {
+  const [command, ...args] = input.split(' ');
+  console.log(args)
+  const command_list = ['ls', 'cd', 'up', 'cat', 'add', 'rm', 'rn','cp','mv','.exit'];
+
+  if (command_list.includes(command)) {
+    if (args.length === 1)  {
+      myEmitter.emit(command, args);
+    } else if (command === '.exit') {
+      rl.close();
+    } else {
+      myEmitter.emit(command, ...args);
+    }
+  } else {
+    console.log('Invalid input');
+    dispCurrentDir()
+  }
 };
