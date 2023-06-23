756055a4e8f0ab6ebc368724e610d2293dbef27a
Oleg Semenov <semoleg1986@gmail.com>
2023-06-24 02:29:34 +0300

    feat: update func cd
    cd - return homedir
    cd [dir] - go to dir

--- src/handler/cd.js
+++ src/handler/cd.js
@@ -1,10 +1,16 @@
-import { dispCurrentDir } from "../helper/index.js"
+import { dispCurrentDir } from "../helper/index.js";
+import os from 'os';
 
-export const changeDir = async ([pathToDirectory]) => {
+export const changeDir = async ([pathToDirectory]=[]) => {
     try{
-        process.chdir(pathToDirectory)
+        if (!pathToDirectory || pathToDirectory.length === 0){
+            process.chdir(os.homedir())
+        } else {
+            process.chdir(pathToDirectory)
+        }
         dispCurrentDir()
     } catch(err){
         console.error('Operation failed')
+        dispCurrentDir()
     }   
 }
