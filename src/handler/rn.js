b95e41bcfcfca3a00324a0d9e7a3fe10b0da6b47
Oleg Semenov <semoleg1986@gmail.com>
2023-06-24 02:32:07 +0300

    feat: new func renameFile

---
+++ src/handler/rn.js
@@ -0,0 +1,21 @@
+import { promises as fs } from 'fs';
+import { resolve } from "path";
+
+export async function renameFile(oldName, newName){
+    const currentPath = resolve(process.cwd());
+    const oldPath = resolve(currentPath, String(oldName));
+    const newPath = resolve(currentPath, String(newName));
+    try {
+        await fs.access(oldPath, fs.constants.F_OK);
+        
+        try {
+            await fs.stat(newPath);
+            console.error('Operation failed');
+            return; 
+        } catch (error) {
+            await fs.rename(oldPath, newPath);
+        }
+    } catch (error) {
+        console.error('Operation failed');
+    }
+};
