9c7c8955b9485e9ae75c6e0f8236fe56fb156882
Oleg Semenov <semoleg1986@gmail.com>
2023-06-23 10:56:25 +0300

    feat: create add

---
+++ src/handler/add.js
@@ -0,0 +1,18 @@
+import { resolve } from "path";
+import { dispCurrentDir, getCurrentDir } from "../helper/index.js";
+import { promises as fs} from 'fs';
+
+export const addFile = async (fileName) => {
+    let newFile;
+    try {
+      const currentPath = getCurrentDir();
+      const filePath = resolve(currentPath, String(fileName));
+      console.log(filePath);
+      newFile = await fs.open(filePath, 'w');
+      dispCurrentDir();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      newFile?.close();
+    }
+  };
