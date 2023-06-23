d4bc0a581aa5bc4d44eff453c0c9af27cdacd102
Oleg Semenov <semoleg1986@gmail.com>
2023-06-24 02:30:54 +0300

    feat: new func in ls
    now we can pipe other folder
    ls [dir]

--- src/handler/ls.js
+++ src/handler/ls.js
@@ -2,21 +2,43 @@ import { promises as fs} from 'fs';
 import { resolve } from "path"
 import { dispCurrentDir } from '../helper/index.js';
 
-export const list = async () => {
+export const list = async (pathDir) => {
     try {
-        const currentPath = resolve(process.cwd())
+        let currentPath
+        if (pathDir){
+            currentPath = resolve(process.cwd(), String(pathDir));
+        } else {
+            currentPath = resolve(process.cwd());
+        }        
+
         const files = await fs.readdir(currentPath);
         const fileList = [];
+        const folders = [];
+        const sortedFiles = [];
         for (const file of files) {
-          const filePath = resolve(currentPath, file);
-          const stats = await fs.stat(filePath);
-          const fileType = stats.isFile() ? 'File' : 'Directory';
-    
-          fileList.push({ File: file, Type: fileType });
+            const filePath = resolve(currentPath, file);
+            const stats = await fs.stat(filePath);
+            const fileType = stats.isFile() ? 'File' : 'Directory';
+            
+            const fileInfo = {
+                Name: file,
+                Type: fileType,
+              };
+        
+              if (stats.isDirectory()) {
+                folders.push(fileInfo);
+              } else {
+                sortedFiles.push(fileInfo);
+              }
         }
+        sortedFiles.sort((a, b) => a.Name.localeCompare(b.Name));
+        folders.sort((a, b) => a.Name.localeCompare(b.Name));
     
-        console.table(fileList);
-        dispCurrentDir()
+        fileList.push(...folders, ...sortedFiles);
+
+        console.info(currentPath)
+        console.table(fileList, ['Name', 'Type']);
+        dispCurrentDir();
     } 
     catch(err) {
         console.error('Operation failed')
