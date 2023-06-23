e4c08b634fd95df72e0c7691ca413e1e1dc5c1b5
Oleg Semenov <semoleg1986@gmail.com>
2023-06-24 02:28:18 +0300

    fix: update func readFile

--- src/handler/cat.js
+++ src/handler/cat.js
@@ -1,19 +1,25 @@
-import { createReadStream, promises as fs } from 'fs';
-import { resolve } from "path"
-import { pipeline } from 'stream';
+import { createReadStream, constants } from 'fs';
+import { resolve } from 'path';
 import { dispCurrentDir } from '../helper/index.js';
 
 export const readFile = async (fileName) => {
-    try {
-        const currentPath = resolve(process.cwd())
-        const filePath = resolve(currentPath, String(fileName))
-        const readStream = createReadStream(filePath, { encoding: 'utf-8'})
-        // нужно реализовать вывод readstream
-        await pipeline(readStream, process.stdout, () => {
-            dispCurrentDir();
-          });
-    } catch (err) {
-        console.error(err);
-        dispCurrentDir()
-    };
-}
+  try {
+    const currentPath = resolve(process.cwd());
+    const filePath = resolve(currentPath, String(fileName));
+    const readStream = createReadStream(filePath, { encoding: 'utf-8' });
+
+    readStream.on('error', (error) => {
+      console.error('Error reading the file:', error);
+      readStream.destroy();
+    });
+
+    readStream.pipe(process.stdout);
+
+    readStream.on('close', () => {
+      dispCurrentDir();
+    });
+  } catch (error) {
+    console.error('Operation failed');
+    dispCurrentDir();
+  }
+};
