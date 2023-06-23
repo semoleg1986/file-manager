dbbeb780572c7ddc9ee6a257f5cc815d2295d42a
Oleg Semenov <semoleg1986@gmail.com>
2023-06-24 02:33:37 +0300

    fix: delete func getCurrentDirectory

--- src/helper/currentDir.js
+++ src/helper/currentDir.js
@@ -1,11 +1,4 @@
-import {dirname} from 'path';
-
 export function dispCurrentDir() {
     const currentDirectory = process.cwd();
     console.info('You are currently in', currentDirectory);
   }
-
-export function getCurrentDir() {
-  const currentDirectory = process.cwd();
-  return currentDirectory
-}
