8873a41e5e5bd936692a19134c9bdc492a7b0233
Oleg Semenov <semoleg1986@gmail.com>
2023-06-24 02:34:49 +0300

    feat: add quit in type .exit

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ import {
     readFile,
     addFile,
     removeFile,
+    renameFile,
+    copyDir,
+    moveFile,
 } from './handler/index.js';
 import { inputCommand } from './helper/inputCommand.js';
 
+let shouldExit = false;
 
 process.chdir(os.homedir());
 
@@ -28,6 +32,10 @@ myEmitter
     .on('cat', readFile)
     .on('add', addFile)
     .on('rm', removeFile)
+    .on('rn', renameFile)
+    .on('cp', copyDir)
+    .on('mv', moveFile)
+
 
 const startFileManager = async () => {
   const args = process.argv.slice(2);
@@ -42,15 +50,20 @@ const startFileManager = async () => {
   console.log(`Welcome to the File Manager, ${username}!`);
   console.info('You are currently in', os.homedir());
 
-  rl.on('line', (input) => inputCommand(input, myEmitter))
+  rl.on('line', (input) => inputCommand(input, myEmitter, rl))
+    .on('SIGINT', () => {
+      if (shouldExit) {
+        exit();
+      } else {
+        shouldExit = true;
+        console.log('Press Ctrl+C again to exit.');
+      }})
     .on('close', ()=> {
       console.log(`Thank you for using File Manager, ${username}, goodbye!`);
-      process.nextTick(()=> exit());
+      exit();
     });
-
+};
   const exit = () => {
     process.exit(0);
   }
-};
-
 await startFileManager();
