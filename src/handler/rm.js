43cd5f356d68f10123863369cdad1e577e135774
Oleg Semenov <semoleg1986@gmail.com>
2023-06-24 02:31:52 +0300

    fix: update rm
    check to exist file

--- src/handler/rm.js
+++ src/handler/rm.js
@@ -3,12 +3,16 @@ import { resolve } from "path";
 import { dispCurrentDir } from '../helper/index.js';
 
 export const removeFile = async (fileName) => {
-  try {
-    const currentPath = resolve(process.cwd())
-    const filePath = resolve(currentPath, String(fileName))
-    await fs.unlink(filePath);
-    dispCurrentDir()
-  } catch (err) {
-    console.error(err);
-  }
-};
+    try {
+        const currentPath = resolve(process.cwd());
+        const filePath = resolve(currentPath, String(fileName));
+
+        await fs.access(filePath, fs.constants.F_OK);
+
+        await fs.unlink(filePath);
+        dispCurrentDir();
+    } catch (err) {
+        console.error('Operation failed');
+        dispCurrentDir();
+    }
+  };
