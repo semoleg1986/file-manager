ec752504e05a8a12179943da439653818b6bf3b1
Oleg Semenov <semoleg1986@gmail.com>
2023-06-23 10:58:36 +0300

    feat: add new Emitter

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import readline from 'readline';
 import { 
     list,
     changeDir,
-    dirUp, 
+    dirUp,
+    readFile,
+    addFile,
 } from './handler/index.js';
 import { inputCommand } from './helper/inputCommand.js';
 
@@ -16,11 +18,14 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 const myEmitter = new EventEmitter();
+myEmitter.setMaxListeners(0)
 
 myEmitter
     .on('ls', list)
     .on('cd', changeDir)
     .on('up', dirUp)
+    .on('cat', readFile)
+    .on('add', addFile)
 
 const startFileManager = async () => {
   const args = process.argv.slice(2);
